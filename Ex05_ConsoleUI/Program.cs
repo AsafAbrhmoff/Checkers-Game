a69aceeb5ea801dd609879acc10feae3b77baa97
AsafAbrhmoff <42766849+AsafAbrhmoff@users.noreply.github.com>
2018-08-28 13:07:09 +0300

    Update Program.cs

--- Ex05_ConsoleUI/Program.cs
+++ Ex05_ConsoleUI/Program.cs
@@ -5,8 +5,6 @@ using Ex05_UI;
 
 namespace Ex05_CheckerGameForm
 {
-    // $G$ SFN-013 (+6) Bonus: UI with richer graphics / motion / sound.
-
     public class Program
      {
           public static void Main()
